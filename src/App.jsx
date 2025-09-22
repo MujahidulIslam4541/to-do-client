b1751e0e49c8956d86132e2e5ddd3951c9859ac8
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-09-22 21:42:48 +0600

    Initialize my-todo-app with Vite, React, and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <p className="text-xl text-red-700">Hello World! Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati aut, quidem minus perspiciatis enim hic eum magni voluptas amet doloremque ut, nisi laboriosam! Possimus, ratione quam, amet doloribus assumenda dolorum optio reprehenderit incidunt earum officia distinctio vero cupiditate molestiae accusamus, necessitatibus minus nam voluptas ex vitae sapiente adipisci aliquam voluptatem.</p>
+    </>
+  )
+}
+
+export default App
