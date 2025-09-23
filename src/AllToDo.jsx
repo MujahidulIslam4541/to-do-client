5b6ae85e8fbc5c5d3c7974e0b21aa420b078a157
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-09-23 20:26:22 +0600

    Refactor AllToDo component: enhance loading state handling, fix function naming, and improve code formatting

--- src/AllToDo.jsx
+++ src/AllToDo.jsx
@@ -3,49 +3,62 @@ import { IoIosCheckmark } from "react-icons/io";
 import { MdDeleteForever } from "react-icons/md";
 import Loading from "./Loading";
 
-const Table = () => {
+const AllToDo = () => {
     const fetchTodos = async () => {
         const res = await fetch("http://localhost:3000/getItems");
         return res.json();
     };
-
-    const { data = [], isLoading } = useQuery({
+    const { data = [], isLoading, refetch } = useQuery({
         queryKey: ["todos"],
         queryFn: fetchTodos,
     });
     const todos = data?.data || [];
     console.log("API response:", todos);
+
     if (isLoading) {
-        return <Loading></Loading>
+        return <Loading />;
+    }
+
+    const handleCheckMark = async (id) => {
+        await fetch(`http://localhost:3000/update-item/${id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ isCompleted: true }),
+        });
+        refetch()
     };
 
-    const handleCheckMark = (id) => {
-        console.log("Check Mark Clicked", id)
-    }
 
     const handelDeleteToDo = (id) => {
-        console.log("Delete Clicked", id)
-    }
+        console.log("Delete Clicked", id);
+    };
 
     return (
         <>
-
             <div className='flex justify-between m-2'>
                 <h2 className='text-blue-400'>All Task List</h2>
-                <p>Total Task:{todos?.length}</p>
+                <p>Total Task: {todos?.length}</p>
             </div>
 
             {todos?.map((todo) => (
                 <div
                     key={todo._id}
-                    className="flex justify-between rounded-2xl shadow border border-gray-300 p-4 mt-2"
+                    className={`flex justify-between rounded-2xl shadow border border-gray-300 p-4 mt-2
+                     ${todo.isCompleted ? "line-through opacity-70" : ""}
+                    `}
                 >
                     <p>{todo.title}</p>
                     <div className="flex gap-2">
-                        <button onClick={() => handleCheckMark(todo._id)} className="bg-blue-200 rounded-xl text-3xl cursor-pointer">
+                        <button
+                            onClick={() => handleCheckMark(todo._id)}
+                            className="bg-blue-200 rounded-xl text-3xl cursor-pointer"
+                        >
                             <IoIosCheckmark />
                         </button>
-                        <button onClick={()=>handelDeleteToDo(todo._id)} className="bg-red-200 rounded-xl text-3xl cursor-pointer">
+                        <button
+                            onClick={() => handelDeleteToDo(todo._id)}
+                            className="bg-red-200 rounded-xl text-3xl cursor-pointer"
+                        >
                             <MdDeleteForever className="text-red-500" />
                         </button>
                     </div>
@@ -55,4 +68,4 @@ const Table = () => {
     );
 };
 
-export default Table;
+export default AllToDo;
