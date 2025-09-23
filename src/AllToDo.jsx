250d71a52ddec84d9a8751be7820a6d36cade843
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-09-23 17:36:57 +0600

    Add React Query for data fetching and enhance AddToDo and AllToDo components

--- src/AllToDo.jsx
+++ src/AllToDo.jsx
@@ -1,42 +1,51 @@
-import React from 'react'
-import { FaCheck } from 'react-icons/fa'
-import { IoIosCheckmark } from 'react-icons/io'
-import { MdDeleteForever } from 'react-icons/md'
+import { useQuery } from "@tanstack/react-query";
+import { IoIosCheckmark } from "react-icons/io";
+import { MdDeleteForever } from "react-icons/md";
+import Loading from "./Loading";
 
-const table = () => {
+const Table = () => {
+    const fetchTodos = async () => {
+        const res = await fetch("http://localhost:3000/getItems");
+        return res.json();
+    };
+
+    const { data = [], isLoading } = useQuery({
+        queryKey: ["todos"],
+        queryFn: fetchTodos,
+    });
+    const todos = data?.data || [];
+    console.log("API response:", todos);
+    if (isLoading) {
+        return <Loading></Loading>
+    };
+
+
     return (
         <>
-            <div className='flex justify-between rounded-2xl shadow border border-gray-300 p-4 mt-2'>
-                <p>lorem.....</p>
-                <div className='flex gap-2 '>
-                    <button className='bg-blue-200  rounded-xl text-3xl cursor-pointer'><IoIosCheckmark /></button>
-                    <button className='bg-red-200 rounded-xl text-3xl cursor-pointer'><MdDeleteForever className='text-red-500' /></button>
-                </div>
+
+            <div className='flex justify-between m-2'>
+                <h2 className='text-blue-400'>All Task List</h2>
+                <p>Total Task:{todos?.length}</p>
             </div>
 
-
-            <div className='flex justify-between rounded-2xl shadow border border-gray-300 p-4 my-2'>
-                <p>lorem.....</p>
-                <div className='flex gap-2 '>
-                    <button className='bg-blue-200  rounded-xl text-3xl cursor-pointer'><IoIosCheckmark /></button>
-                    <button className='bg-red-200 rounded-xl text-3xl cursor-pointer'><MdDeleteForever className='text-red-500' /></button>
+            {todos?.map((todo) => (
+                <div
+                    key={todo._id}
+                    className="flex justify-between rounded-2xl shadow border border-gray-300 p-4 mt-2"
+                >
+                    <p>{todo.title}</p>
+                    <div className="flex gap-2">
+                        <button className="bg-blue-200 rounded-xl text-3xl cursor-pointer">
+                            <IoIosCheckmark />
+                        </button>
+                        <button className="bg-red-200 rounded-xl text-3xl cursor-pointer">
+                            <MdDeleteForever className="text-red-500" />
+                        </button>
+                    </div>
                 </div>
-            </div>
-
-
-            <div className='flex justify-between rounded-2xl shadow border border-gray-300 p-4 '>
-                <p>lorem.....</p>
-                <div className='flex gap-2 '>
-                    <button className='bg-blue-200  rounded-xl text-3xl cursor-pointer'><IoIosCheckmark /></button>
-                    <button className='bg-red-200 rounded-xl text-3xl cursor-pointer'><MdDeleteForever className='text-red-500' /></button>
-                </div>
-            </div>
-
+            ))}
         </>
+    );
+};
 
-
-
-    )
-}
-
-export default table
+export default Table;
---
+++ src/Loading.jsx
@@ -0,0 +1,13 @@
+import { FadeLoader } from "react-spinners"
+
+
+const Loading = () => {
+    return (
+        <div className="flex justify-center items-center mt-20">
+            <FadeLoader color="#51A2FF" />
+        </div>
+    )
+}
+
+
+export default Loading
